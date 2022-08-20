1d70286d6a4c743d9229fc272b1f80adede67f73
muathye <muath.ye@gmail.com>
2022-08-20 05:37:14 +0300

    Add PWA assets

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,26 @@
 import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Muath Alsowadi',
+        short_name: 'Muathye',
+        description: 'Founder of @open-sale. On a mission to improve programming quality..',
+        theme_color: '#ffc517',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ]
 })
