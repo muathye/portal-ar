f399d85af6c8ca7195c1a24152adac42d7b5ca71
muathye <muath.ye@gmail.com>
2022-10-16 13:59:01 +0300

    :art: update

---
+++ docs/.vitepress/dist/assets/articles.md.6e806dfa.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as s}from"./app.26270015.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"doc"},"headers":[],"relativePath":"articles.md"}'),i={name:"articles.md"},c=s("",3),r=[c];function n(o,_,l,d,p,m){return a(),t("div",null,r)}const h=e(i,[["render",n]]);export{f as __pageData,h as default};
