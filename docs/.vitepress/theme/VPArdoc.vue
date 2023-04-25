0d9c8bed94d064acb82b4f1ddd537eab9f9a8606
muathye <muath.ye@gmail.com>
2023-04-26 02:56:55 +0300

    feat :zap: update the design

---
+++ docs/.vitepress/theme/VPArdoc.vue
@@ -0,0 +1,33 @@
+<!--.vitepress/theme/MyLayout.vue-->
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+const lang = 'ar'
+</script>
+
+<template>
+    <Layout>
+        <template #layout-top>
+            <!-- https://vitepress.dev/guide/extending-default-theme -->
+        </template>
+    </Layout>
+</template>
+<style>
+body {
+    background: url('/images/background/docs-dark@tinypng.1bbe175e.png');
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
+.VPFooter[data-v-80147a0d],
+.VPNav.no-sidebar[data-v-9397ee4e] {
+    background: rgba(255, 255, 255, 0.2);
+}
+
+.dark .VPFooter[data-v-80147a0d],
+.dark .VPNav.no-sidebar[data-v-9397ee4e] {
+    background: rgba(36, 36, 36, 0.2);
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,11 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
+import VPArdoc from './VPArdoc.vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  // override the Layout with a wrapper component that
+  // injects the slots
+  Layout: VPArdoc,
+}
