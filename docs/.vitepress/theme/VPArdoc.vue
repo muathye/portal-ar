d29e508fd878edd4874cf1e2370e9a628faf4808
muathye <muath.ye@gmail.com>
2023-04-26 03:00:30 +0300

    feat :zap: update the design

--- docs/.vitepress/theme/VPArdoc.vue
+++ docs/.vitepress/theme/VPArdoc.vue
@@ -13,21 +13,3 @@ const lang = 'ar'
         </template>
     </Layout>
 </template>
-<style>
-body {
-    background: url('/images/background/docs-dark@tinypng.1bbe175e.png');
-    height: 100vh;
-    background-position: center;
-    background-repeat: no-repeat;
-}
-
-.VPFooter[data-v-80147a0d],
-.VPNav.no-sidebar[data-v-9397ee4e] {
-    background: rgba(255, 255, 255, 0.2);
-}
-
-.dark .VPFooter[data-v-80147a0d],
-.dark .VPNav.no-sidebar[data-v-9397ee4e] {
-    background: rgba(36, 36, 36, 0.2);
-}
-</style>
