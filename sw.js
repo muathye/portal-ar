72cb6c261f0952c78283973df191bf8746b5703d
muath-ye <muath-ye@users.noreply.github.com>
2023-05-18 06:54:06 +0000

    deploy: 5238fc97bb9d6a5c26ea361d039d77859886fcd9

---
+++ sw.js
@@ -0,0 +1,336 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-3589c0c5'], (function (workbox) { 'use strict';
+
+  /**
+  * Welcome to your Workbox-powered service worker!
+  *
+  * You'll need to register this file in your web app.
+  * See https://goo.gl/nhQhGp
+  *
+  * The rest of the code is auto-generated. Please don't update this file
+  * directly; instead, make changes to your Workbox build configuration
+  * and re-run your build process.
+  * See https://goo.gl/2aRDsh
+  */
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+
+  workbox.precacheAndRoute([{
+    "url": "404.html",
+    "revision": "c832450809cbf8b459bb76eac19823d7"
+  }, {
+    "url": "android-chrome-192x192.png",
+    "revision": "8b6f412bda677ba08ec46492d2dbbb23"
+  }, {
+    "url": "android-chrome-512x512.png",
+    "revision": "951e66101f8ab8db77e9a2986a28af9a"
+  }, {
+    "url": "apple-touch-icon.png",
+    "revision": "25ab1e6e2615ec76f1febb7ecacf1ba4"
+  }, {
+    "url": "ar/index.html",
+    "revision": "dbd9d9df29e561d1355dd1390cf4a283"
+  }, {
+    "url": "articles.html",
+    "revision": "04e60c8a11bb3201cff299f8386a3304"
+  }, {
+    "url": "articles/2022-08-20-gitmoji.html",
+    "revision": "f937e5258e85f8507cf23ad9ce42e744"
+  }, {
+    "url": "articles/2022-12-26/modal-datetime-picker.png",
+    "revision": "15769119e7cce9ba9a116fac03e9f5d6"
+  }, {
+    "url": "articles/2022-12-27/morning-azkar.png",
+    "revision": "6f92ec237639cc9874eef93d02fd24ec"
+  }, {
+    "url": "articles/2023-03-08/mini-self-hosted-verison-chatgpt.png",
+    "revision": "a20d809a2b93470f70bed0c150f3502c"
+  }, {
+    "url": "articles/2023-04-24-clone-private-github-repository-using-ssh.html",
+    "revision": "6448af8fc6dd012b8c8da3a8eceae843"
+  }, {
+    "url": "articles/2023-04-24/add-deploy-key.png",
+    "revision": "d3f730d15093bdf2f47998f05704b31d"
+  }, {
+    "url": "articles/2023-04-24/deploy-key.png",
+    "revision": "377de964ceb368d49736a0fe7a55e89a"
+  }, {
+    "url": "articles/2023-04-24/deploy-keys.png",
+    "revision": "930fb68e72b4cb135cab92b7d1cfca3c"
+  }, {
+    "url": "articles/2023-04-24/ssh-clone-url.png",
+    "revision": "104c5e43ba39312783c03db8f71d3e69"
+  }, {
+    "url": "articles/2023-04-25-solve-the-page-expired-419-error-in-laravel.html",
+    "revision": "b977a87e2dd5a84ca2089fa9421d8402"
+  }, {
+    "url": "articles/2023-04-25/419-page-expired.png",
+    "revision": "9d61d2526077ba165148c154ab8853a7"
+  }, {
+    "url": "assets/app.d4e38dc9.js",
+    "revision": null
+  }, {
+    "url": "assets/ar_index.md.7878dbb1.js",
+    "revision": null
+  }, {
+    "url": "assets/ar_index.md.7878dbb1.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-08-20-gitmoji.md.1852339d.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-08-20-gitmoji.md.1852339d.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-24-clone-private-github-repository-using-ssh.md.86016d7f.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-24-clone-private-github-repository-using-ssh.md.86016d7f.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-25-solve-the-page-expired-419-error-in-laravel.md.c5f52ef1.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-25-solve-the-page-expired-419-error-in-laravel.md.c5f52ef1.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles.md.908aec44.js",
+    "revision": null
+  }, {
+    "url": "assets/articles.md.908aec44.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/index.md.27cf758d.js",
+    "revision": null
+  }, {
+    "url": "assets/index.md.27cf758d.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/inter-cyrillic-ext.0877b0d9.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-cyrillic.f8750142.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-greek-ext.3e6f6728.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-greek.117e1956.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-cyrillic-ext.33bd5a8e.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-cyrillic.ea42a392.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-greek-ext.4fbe9427.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-greek.8f4463c4.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-latin-ext.bd8920cc.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-latin.bd3b6f56.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-vietnamese.6ce511fb.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-latin-ext.7cc429bc.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-latin.4fe6132f.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-cyrillic-ext.e75737ce.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-cyrillic.5f2c6c8c.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-greek-ext.ab0619bc.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-greek.d5a6d92a.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-latin-ext.0030eebd.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-latin.2ed14f66.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-vietnamese.14ce25a6.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-vietnamese.2c644a25.woff2",
+    "revision": null
+  }, {
+    "url": "assets/projects.md.39f30780.js",
+    "revision": null
+  }, {
+    "url": "assets/projects.md.39f30780.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/resume.md.cf3475de.js",
+    "revision": null
+  }, {
+    "url": "assets/resume.md.cf3475de.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/style.689013c9.css",
+    "revision": null
+  }, {
+    "url": "favicon-16x16.png",
+    "revision": "d4de349fd29d46f50aeac6cb294ae991"
+  }, {
+    "url": "favicon-32x32.png",
+    "revision": "8a8623ddf50c485ce105569baa5c2afd"
+  }, {
+    "url": "favicon.ico",
+    "revision": "d3091d363d8b52c41c1f227d8105dfaf"
+  }, {
+    "url": "images/background/docs-dark@tinypng.1bbe175e.png",
+    "revision": null
+  }, {
+    "url": "images/muathye.png",
+    "revision": "da6a18db9204b5d051aaf7e225105b8b"
+  }, {
+    "url": "images/muathye.svg",
+    "revision": "8b8e61771570c80c5470b1adaf2c44ee"
+  }, {
+    "url": "index.html",
+    "revision": "b81e813d6b9ff4b2e17996505f8be269"
+  }, {
+    "url": "masked-icon.svg",
+    "revision": "8b8e61771570c80c5470b1adaf2c44ee"
+  }, {
+    "url": "mstile-150x150.png",
+    "revision": "f020256b2109323117574ed955e15b13"
+  }, {
+    "url": "projects.html",
+    "revision": "03b8c7f75c1be321bb26e83a0763c75f"
+  }, {
+    "url": "projects/eazycare.app/new-logo.svg",
+    "revision": "8bcc0aef753052adae872ffae4e45ac4"
+  }, {
+    "url": "projects/mokasweets.com/mokasweets-logo.svg",
+    "revision": "c70f81c8d6ac2f8a1b670ef749cfb820"
+  }, {
+    "url": "projects/OpenSale/open-sale.svg",
+    "revision": "a7590fa654f4499d1d36ad147630556d"
+  }, {
+    "url": "projects/sdelivery.co/dark.png",
+    "revision": "47b561ffb45d803e592be20b255f7b00"
+  }, {
+    "url": "projects/sdelivery.co/light.png",
+    "revision": "da1ff181df734f37f8e3c13dbab91dbc"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "8b6f412bda677ba08ec46492d2dbbb23"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "951e66101f8ab8db77e9a2986a28af9a"
+  }, {
+    "url": "registerSW.js",
+    "revision": "1872c500de691dce40960bb85481de07"
+  }, {
+    "url": "resume.html",
+    "revision": "e65053ccdcf8143eeb0d7a86416ddc7d"
+  }, {
+    "url": "robots.txt",
+    "revision": "5e0bd1c281a62a380d7a948085bfe2d1"
+  }, {
+    "url": "safari-pinned-tab.svg",
+    "revision": "896ff3300aa31eae3864243b6f116f66"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "8b6f412bda677ba08ec46492d2dbbb23"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "951e66101f8ab8db77e9a2986a28af9a"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "b1eec4280dc7c7f0bca6689acb26fb4b"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
